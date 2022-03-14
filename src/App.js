cbc1ab8bfac79441a095018024fc00ce4df1607b
adreider <adreidereconomic@gmail.com>
2022-03-14 16:37:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [name, setName] = useState('Adriano');
+
+  function HandleAddName() {
+    setName('Clarinha')
+  }
+
+  useEffect(() => {
+    console.log('useEffect com dependencia vazia')
+  }, [])
+
+  useEffect(() => {
+    console.log('useEffect com dependencia [name]')
+  }, [name])
+
+  useEffect(() => {
+    console.log('useEffect sem dependencia')
+  })
+
+  console.log('render')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{name}</h1>
+
+      <button onClick={HandleAddName}>Add Name</button>
+    </>
   );
 }
 
